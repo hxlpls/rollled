0df084c863405a5dd7e7d7d9a2e5d58cfed39ec2
hxlpls <106937850+hxlpls@users.noreply.github.com>
2023-12-30 14:21:44 +0300

    Add files via upload

---
+++ webtasarim/webtasarim/Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=home}/{action=index}"
+    );
+
+app.Run();
